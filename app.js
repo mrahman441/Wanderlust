ee91a61e77530203ceb6ae1da262b019a7f5eec1
Mahbub <mrahman221441@gmail.com>
2025-12-25 01:27:54 +0600

    complete listing

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate); // ejs-mate for layouts
-app.use(express.static(path.join(__dirname, "/public")));
 
 
 // api routes
