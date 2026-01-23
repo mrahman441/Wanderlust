4bddf1094ffbb4da9e654a316c1a2c4bdf84adfe
Mahbub <mrahman221441@gmail.com>
2026-01-23 15:50:42 +0600

    strucure api

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router({ mergeParams: true });
+const router = express.Router({ mergeParams: true }); // mergeParams to access :id from parent route
 const wrapAsync = require('../utils/wrapAsync.js');
 const { validateReview } = require('../utils/validate.js');
 
