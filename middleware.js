d3ecc86e5afb99265ebd97037691fe702456fb65
Mahbub <mrahman221441@gmail.com>
2026-03-03 19:12:00 +0600

    img-upload

--- middleware.js
+++ middleware.js
@@ -5,7 +5,16 @@ const Review = require("./models/review");
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         // store the url they are requesting. so that after they login, we can redirect them back to that url.
-        req.session.returnTo = req.originalUrl;
+        
+        // but don't store POST routes, instead redirect to a sensible GET route
+        if (req.method === 'POST' && req.originalUrl.includes('/reviews')) {
+            // For review POST requests, redirect to the listing page after login
+            const listingUrl = req.originalUrl.replace(/\/reviews.*$/, '');
+            req.session.returnTo = listingUrl;
+        } else if (req.method === 'GET') {
+            req.session.returnTo = req.originalUrl;
+        }
+        // For other POST/PUT/DELETE methods, don't set returnTo (will go to /listings by default)
 
         req.flash("error", "You must be signed in to create a listing!");
         return res.redirect("/auth/login");
