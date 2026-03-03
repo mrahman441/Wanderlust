3fd726d4c5e1b61ea7e0d090b9b5e2e9a3e3cdcc
Mahbub <mrahman221441@gmail.com>
2026-03-03 15:53:07 +0600

    cloudinary setup

--- routes/listings.js
+++ routes/listings.js
@@ -5,18 +5,21 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const { validateListing } = require('../utils/validate.js');
 const { isLoggedIn, isOwner } = require('../middleware.js');
 
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 // import controllers
 const listings = require('../controllers/listings.js');
 
 router.route('/')
     .get(wrapAsync(listings.index)) // show all listings
-    .post(isLoggedIn, validateListing, wrapAsync(listings.createListing)); // create listing
+    .post(isLoggedIn, validateListing, upload.single('listing[image]'), wrapAsync(listings.createListing)); // create listing
 
 router.get('/new', isLoggedIn, listings.renderNewForm);  // new listing form
 
 router.route('/:id')
     .get(wrapAsync(listings.showListing)) // Show listing details
-    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listings.updateListing)) // Update listing
+    .put(isLoggedIn, isOwner, validateListing, upload.single('listing[image]'), wrapAsync(listings.updateListing)) // Update listing
     .delete(isLoggedIn, isOwner, wrapAsync(listings.deleteListing)); // Delete listing
 
 router.get('/:id/edit', isLoggedIn, isOwner, wrapAsync(listings.renderEditForm)); // Edit listing
